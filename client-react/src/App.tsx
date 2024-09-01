52bb333d378cd0f2eb75d35cb751b92c5dd958a3
M Alejandra Rojas M <113424427+alejandra-rojas@users.noreply.github.com>
2024-09-01 17:04:46 +0200

    router squash
    package: tanstack/router
    
    package: -D router-vite-plgind & devtools
    
    add: vite router config
    
    router: createRouter + routes
    
    add: routes
    
    add: property path parameters
    
    styles: user button

--- client-react/src/App.tsx
+++ client-react/src/App.tsx
@@ -1,9 +1,16 @@
+import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
+
+const router = createRouter({ routeTree });
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
 function App() {
-  return (
-    <>
-      <div>My Real Estate App</div>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
