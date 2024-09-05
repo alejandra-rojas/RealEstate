244bfc7e1e71612af12960555d2ac322376c5a17
M Alejandra Rojas M <113424427+alejandra-rojas@users.noreply.github.com>
2024-09-05 16:36:08 +0200

    add: Agent ID

--- webApi/Models/Agent.cs
+++ webApi/Models/Agent.cs
@@ -2,6 +2,7 @@ namespace webApi.Models;
 
 public class Agent
 {
+    public int AgentId { get; set; }
     public required string Name;
     public required string PrimaryNumber;
     public string? Photo;
