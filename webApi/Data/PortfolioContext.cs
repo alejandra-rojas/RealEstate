673b91b2be608188e6db2da1b2174349c200b4f7
M Alejandra Rojas M <113424427+alejandra-rojas@users.noreply.github.com>
2024-09-05 17:20:27 +0200

    add: tables to context

--- webApi/Data/PortfolioContext.cs
+++ webApi/Data/PortfolioContext.cs
@@ -3,6 +3,9 @@ using webApi.Models;
 
 public class PortfolioContext(DbContextOptions<PortfolioContext> options) : DbContext(options)
 {
-    public DbSet<Property> Properties { get; set; } = default!;
+    public DbSet<Property> Properties { get; set; }
+    public DbSet<Agent> Agents { get; set; }
+    public DbSet<Seller> Sellers { get; set; }
+    public DbSet<PropertyDetails> PropertyDetails { get; set; }
 
 }
