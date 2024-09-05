7b646d9a438bd3714b75de9177b614b5e9b36dec
M Alejandra Rojas M <113424427+alejandra-rojas@users.noreply.github.com>
2024-09-05 17:06:19 +0200

    refactor: scaffolded template

--- webApi/Controllers/PropertiesController.cs
+++ webApi/Controllers/PropertiesController.cs
@@ -19,14 +19,14 @@ namespace webApi.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Property>>> GetProperty()
         {
-            return await _context.Property.ToListAsync();
+            return await _context.Properties.ToListAsync();
         }
 
         // GET: api/Properties/5
         [HttpGet("{id}")]
         public async Task<ActionResult<Property>> GetProperty(int id)
         {
-            var @property = await _context.Property.FindAsync(id);
+            var @property = await _context.Properties.FindAsync(id);
 
             if (@property == null)
             {
@@ -72,7 +72,7 @@ namespace webApi.Controllers
         [HttpPost]
         public async Task<ActionResult<Property>> PostProperty(Property @property)
         {
-            _context.Property.Add(@property);
+            _context.Properties.Add(@property);
             await _context.SaveChangesAsync();
 
             return CreatedAtAction("GetProperty", new { id = @property.PropertyId }, @property);
@@ -82,13 +82,13 @@ namespace webApi.Controllers
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteProperty(int id)
         {
-            var @property = await _context.Property.FindAsync(id);
+            var @property = await _context.Properties.FindAsync(id);
             if (@property == null)
             {
                 return NotFound();
             }
 
-            _context.Property.Remove(@property);
+            _context.Properties.Remove(@property);
             await _context.SaveChangesAsync();
 
             return NoContent();
@@ -96,7 +96,7 @@ namespace webApi.Controllers
 
         private bool PropertyExists(int id)
         {
-            return _context.Property.Any(e => e.PropertyId == id);
+            return _context.Properties.Any(e => e.PropertyId == id);
         }
     }
 }
--- webApi/Data/PortfolioContext.cs
+++ webApi/Data/PortfolioContext.cs
@@ -1,16 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using webApi.Models;
 
-    public class PortfolioContext : DbContext
-    {
-        public PortfolioContext (DbContextOptions<PortfolioContext> options)
-            : base(options)
-        {
-        }
+public class PortfolioContext(DbContextOptions<PortfolioContext> options) : DbContext(options)
+{
+    public DbSet<Property> Properties { get; set; } = default!;
 
-        public DbSet<webApi.Models.Property> Property { get; set; } = default!;
-    }
+}
