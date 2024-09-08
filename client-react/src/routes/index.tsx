c231bba727cbd017e50949218268c4cdd0a545c8
M Alejandra Rojas M <113424427+alejandra-rojas@users.noreply.github.com>
2024-09-08 14:20:19 +0200

    add: property cards (basic)

--- client-react/src/routes/index.tsx
+++ client-react/src/routes/index.tsx
@@ -1,6 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
-import { Property } from "../types/types";
+import { Property, Status } from "../types/types";
 import { fetchPublicPortfolio } from "../utils/api";
 
 export const Route = createFileRoute("/")({
@@ -24,13 +24,69 @@ function Homepage() {
 
   return (
     <>
+      <img
+        className="p-1.5 h-auto object-cover"
+        src="src/assets/homepage-hero.jpg"
+        alt="image-hero"
+      />
       {isFetched && (
-        <img
-          className="p-1.5 h-auto object-cover"
-          src="src/assets/homepage-hero.jpg"
-          alt="image-hero"
-        />
+        <div>
+          <FilterablePortfolio portfolio={portfolio ?? []} />
+        </div>
       )}
     </>
+  );
+}
+
+function FilterablePortfolio({ portfolio }: { portfolio: Property[] }) {
+  return (
+    <>
+      {portfolio.map((p) => {
+        return (
+          <PropertyCard
+            createdAt={p.createdAt}
+            salePrice={p.salePrice}
+            propertyId={p.propertyId}
+            status={p.status}
+            propertyName={p.propertyName}
+            address={p.address}
+            landSizeInSquareMeters={p.landSizeInSquareMeters}
+            constructionSizeInSquareMeters={p.constructionSizeInSquareMeters}
+            photo={p.photo}
+            numberOfRooms={p.numberOfRooms}
+            description={p.description}
+            key={p.propertyId}
+          />
+        );
+      })}
+    </>
+  );
+}
+
+function PropertyCard({
+  createdAt,
+  salePrice,
+  status,
+  propertyName,
+  address,
+  landSizeInSquareMeters,
+  constructionSizeInSquareMeters,
+  numberOfRooms,
+  description,
+  photo,
+}: Property) {
+  return (
+    <div>
+      <h3>{propertyName}</h3>
+      <p>{address}</p>
+      <p>{description}</p>
+      <p>Price: ${salePrice}</p>
+      <p>Status: {Status[status]}</p>
+      <img src={photo} alt={propertyName} />
+      <p>Rooms: {numberOfRooms}</p>
+      <p>Land Size: {landSizeInSquareMeters} sqm</p>
+      <p>Construction Size: {constructionSizeInSquareMeters} sqm</p>
+      <p>Created At: {createdAt}</p>
+    </div>
   );
 }
