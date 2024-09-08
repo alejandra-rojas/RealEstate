77e3278fdec026b60d7b7a5f3f9685b0773f8323
M Alejandra Rojas M <113424427+alejandra-rojas@users.noreply.github.com>
2024-09-08 16:29:39 +0200

    add: Files component

--- client-react/src/routes/files/index.tsx
+++ client-react/src/routes/files/index.tsx
@@ -1,14 +1,59 @@
 import { createFileRoute, Link } from "@tanstack/react-router";
+import { File, Status } from "../../types/types";
+import { useQuery } from "@tanstack/react-query";
+import { fetchFiles } from "../../utils/api";
 
 export const Route = createFileRoute("/files/")({
   component: Files,
 });
 
 function Files() {
+  const {
+    error,
+    data: files,
+    isFetched,
+  } = useQuery<File[]>({
+    queryKey: ["files"],
+    queryFn: fetchFiles,
+  });
+
+  console.log(files);
+
+  if (error) return "An error has occurred while fetching : " + error.message;
+
   return (
-    <div>
-      Hello /files!
-      <Link to={`/files/4`}>hey</Link>
-    </div>
+    <>
+      {isFetched && (
+        <div>
+          <FilterableFiles files={files ?? []} />
+        </div>
+      )}
+    </>
+  );
+}
+
+function FilterableFiles({ files }: { files: File[] }) {
+  return (
+    <>
+      <div className="flex flex-col gap-4 border">
+        {files.map((file) => {
+          return <FileCard key={file.propertyId} file={file} />;
+        })}
+      </div>
+    </>
+  );
+}
+
+function FileCard({ file }: { file: File }) {
+  return (
+    <Link to={`/files/${file.propertyId}`}>
+      <div>
+        <h3>{file.propertyDetails.propertyName}</h3>
+        <p>Price: ${file.salePrice}</p>
+        <p>Status: {Status[file.status]}</p>
+        <p>Seller: {file.seller.fullName}</p>
+        <p>Agent: {file.propertyLiasonAgent.name} sqm</p>
+      </div>
+    </Link>
   );
 }
