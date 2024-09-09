ba6e5010e9c85cd49ca8ff0fc72e1f70579fd788
M Alejandra Rojas M <113424427+alejandra-rojas@users.noreply.github.com>
2024-09-09 16:59:18 +0200

    add: Dashboard Header

---
+++ client-react/src/features/Dashboard/BusinessPerformance.tsx
@@ -0,0 +1,9 @@
+function BusinessPerformance() {
+  return (
+    <div>
+      BusinessPerformance to show key metrics, as in numbers/commision so far
+    </div>
+  );
+}
+
+export default BusinessPerformance;
--- client-react/src/routes/dashboard.tsx
+++ client-react/src/routes/dashboard.tsx
@@ -4,6 +4,8 @@ import { createFileRoute } from "@tanstack/react-router";
 import { fetchFiles } from "../utils/api";
 import UpcomingEvents from "../features/Dashboard/UpcomingEvents";
 import OngoingTransactions from "../features/Dashboard/OngoingTransactions";
+import BusinessPerformance from "../features/Dashboard/BusinessPerformance";
+import Header from "../features/Dashboard/Header";
 
 export const Route = createFileRoute("/dashboard")({
   component: Dashboard,
@@ -20,7 +22,8 @@ function Dashboard() {
   return (
     isFetched && (
       <section>
-        <h2>Business Overview</h2>
+        <Header />
+        <BusinessPerformance />
         <UpcomingEvents files={files!} />
         <OngoingTransactions />
       </section>
