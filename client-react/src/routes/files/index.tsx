0fc4ba2eabda0bbeb1936a721c52f33e983ae9b7
M Alejandra Rojas M <113424427+alejandra-rojas@users.noreply.github.com>
2024-09-09 13:54:49 +0200

    architecture: refactor - components breakdown

--- client-react/src/routes/files/index.tsx
+++ client-react/src/routes/files/index.tsx
@@ -1,7 +1,9 @@
-import { createFileRoute, Link } from "@tanstack/react-router";
-import { File, Status } from "../../types/types";
+import { createFileRoute } from "@tanstack/react-router";
+import { File } from "../../types/types";
 import { useQuery } from "@tanstack/react-query";
 import { fetchFiles } from "../../utils/api";
+import FilterBar from "../../features/Files/FilterBar";
+import FilteredFiles from "../../features/Files/FilteredFiles";
 
 export const Route = createFileRoute("/files/")({
   component: Files,
@@ -32,35 +34,10 @@ function Files() {
     <>
       {isFetched && (
         <div>
-          <FilterableFiles files={sortByCreationDate ?? []} />
+          <FilterBar />
+          <FilteredFiles files={sortByCreationDate ?? []} />
         </div>
       )}
     </>
-  );
-}
-
-function FilterableFiles({ files }: { files: File[] }) {
-  return (
-    <>
-      <div className="flex flex-col gap-4 border">
-        {files.map((file) => {
-          return <FileCard key={file.propertyId} file={file} />;
-        })}
-      </div>
-    </>
-  );
-}
-
-function FileCard({ file }: { file: File }) {
-  return (
-    <Link to={`/files/${file.propertyId}`}>
-      <div>
-        <h3>{file.propertyDetails.propertyName}</h3>
-        <p>Price: ${file.salePrice}</p>
-        <p>Status: {Status[file.status]}</p>
-        <p>Seller: {file.seller.fullName}</p>
-        <p>Agent: {file.propertyLiasonAgent.name}</p>
-      </div>
-    </Link>
   );
 }
