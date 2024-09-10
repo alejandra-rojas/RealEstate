acd7b2fd0e31dac01f2ee6178e752bb939f71a0a
M Alejandra Rojas M <113424427+alejandra-rojas@users.noreply.github.com>
2024-09-10 10:26:36 +0200

    styling: navigation

--- client-react/src/routes/__root.tsx
+++ client-react/src/routes/__root.tsx
@@ -18,55 +18,82 @@ const activeProps = {
 export const Route = createRootRoute({
   component: () => (
     <>
-      <header>
+      <header className="pb-4">
         <div className="flex justify-between">
-          <div className="flex gap-10">
-            <h1>RealEstate Mgmt</h1>
+          <div className="flex pl-2 gap-10 items-start">
+            <div className="flex flex-col font-rmono text-almostblack cursor-default">
+              <h1 className="font-bold uppercase text-xl leading-none">
+                4RealState
+              </h1>
+              <h2 className="text-xs">Property Management</h2>
+            </div>
             <nav>
               <ul className="flex gap-4">
                 <li>
                   <Link to="/" activeProps={activeProps}>
-                    Homepage
+                    Portfolio
                   </Link>
                 </li>
+                <li className="cursor-default">About</li>
+                <li className="cursor-default">Contact</li>
               </ul>
             </nav>
           </div>
-          <div>
-            <SignedOut>
-              <SignInButton forceRedirectUrl="/dashboard" />
-            </SignedOut>
+          <div className="flex items-center">
+            <div>
+              <SignedIn>
+                <nav id="admin-nav" className="flex justify-between gap-6">
+                  <ul className="flex gap-4">
+                    <li className="uppercase font-rmono bg-bggray py-1 px-2 text-almostblack">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={2}
+                        stroke="currentColor"
+                        className="size-5 pt-1"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0"
+                        />
+                      </svg>
+                    </li>
+
+                    <li className="uppercase font-rmono bg-bggray hover:bg-accent py-1 px-2.5 text-almostblack">
+                      <Link to="/files" activeProps={activeProps}>
+                        Files
+                      </Link>
+                    </li>
+                    <li className="uppercase font-rmono bg-bggray hover:bg-accent py-1 px-2.5 text-almostblack">
+                      <Link to="/dashboard" activeProps={activeProps}>
+                        Dashboard
+                      </Link>
+                    </li>
+                  </ul>
+
+                  <UserButton
+                    appearance={{
+                      elements: {
+                        userButtonAvatarBox: "w-7 h-7",
+                      },
+                    }}
+                  />
+                </nav>
+              </SignedIn>
+            </div>
+            <div>
+              <SignedOut>
+                <SignInButton forceRedirectUrl="/dashboard">
+                  <button className="uppercase font-rmono bg-bggray hover:bg-accent py-1 px-2.5 text-almostblack">
+                    Admin
+                  </button>
+                </SignInButton>
+              </SignedOut>
+            </div>
           </div>
         </div>
-        <SignedIn>
-          <nav id="admin-nav" className="flex justify-between">
-            <ul className="flex gap-4">
-              <li>
-                <Link to="/dashboard" activeProps={activeProps}>
-                  Dashboard
-                </Link>
-              </li>
-              <li>
-                <Link to="/files" activeProps={activeProps}>
-                  Files
-                </Link>
-              </li>
-              <li>
-                <Link to="/new" activeProps={activeProps}>
-                  New
-                </Link>
-              </li>
-            </ul>
-
-            <UserButton
-              appearance={{
-                elements: {
-                  userButtonAvatarBox: "w-8 h-8",
-                },
-              }}
-            />
-          </nav>
-        </SignedIn>
       </header>
       <ToastContainer />
       <Outlet />
