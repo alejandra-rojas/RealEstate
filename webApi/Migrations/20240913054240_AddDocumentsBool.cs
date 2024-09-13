c53b7f4f29803fb3ffa59382aae04a6e70c907d6
M Alejandra Rojas M <113424427+alejandra-rojas@users.noreply.github.com>
2024-09-13 08:12:30 +0200

    migration: has docs

---
+++ webApi/Migrations/20240913054240_AddDocumentsBool.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace webApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDocumentsBool : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "HasDocuments",
+                table: "Sellers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "HasDocuments",
+                table: "Properties",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "HasDocuments",
+                table: "Buyers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HasDocuments",
+                table: "Sellers");
+
+            migrationBuilder.DropColumn(
+                name: "HasDocuments",
+                table: "Properties");
+
+            migrationBuilder.DropColumn(
+                name: "HasDocuments",
+                table: "Buyers");
+        }
+    }
+}
