f2ec22c9d6bef1a39de4ae36bac325b1f69bb084
M Alejandra Rojas M <113424427+alejandra-rojas@users.noreply.github.com>
2024-10-02 17:28:54 +0200

    add: Controller Tests

---
+++ webApi.Tests/PropertiesControllerTests.cs
@@ -0,0 +1,23 @@
+using System.Net;
+using System.Net.Http.Json;
+using FluentAssertions;
+using webApi.Models;
+
+
+namespace webApi.Tests;
+
+public class PropertiesControllersTests(webApiFactory factory) : IClassFixture<webApiFactory>
+{
+
+    private readonly HttpClient _client = factory.CreateClient();
+
+    [Fact]
+    public async Task GetAll_ShouldReturn16Properties()
+    {
+        // Act
+        var response = await _client.GetFromJsonAsync<List<Property>>("/api/Properties");
+
+        // Assert
+        response!.Count.Should().BeGreaterThanOrEqualTo(16);
+    }
+}
